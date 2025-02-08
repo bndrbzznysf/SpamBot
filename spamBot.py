11963fbc97a08d6598c4257cd654e6a93c6735e3
Ines <158498889+bndrbzznysf@users.noreply.github.com>
2025-02-08 22:39:40 +0100

    Update spamBot.py

--- spamBot.py
+++ spamBot.py
@@ -6,7 +6,7 @@ import webbrowser
 def sendSpam():
     words = open('spam.txt', 'r')
     webbrowser.open('https://web.whatsapp.com/')
-    time.sleep(10)
+    time.sleep(20)
     for word in words:
         auto.typewrite(word)
         auto.press('enter')
